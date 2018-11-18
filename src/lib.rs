20fd30ce442716a2691c34e373fe24b3e3af8828
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2018-11-18 19:45:45 +0100

    pub

--- src/lib.rs
+++ src/lib.rs
@@ -8,14 +8,13 @@ pub mod macros;
 mod map;
 mod number;
 #[cfg(feature = "serde_enc")]
-mod serde_enc;
+pub mod serde_enc;
 mod to_value;
 mod value;
 
 pub use self::map::*;
 pub use self::number::*;
 #[cfg(feature = "serde_enc")]
-pub use self::serde_enc::*;
 pub use self::to_value::ToValue;
 pub use self::value::*;
 
